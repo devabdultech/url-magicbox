067b01b5bf19e590d02c509057b0787a6e1f64a1
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-07-06 17:09:16 +0100

    added expiration date to the links generated

--- src/models/Url.ts
+++ src/models/Url.ts
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 
-const urlSchema = new mongoose.Schema({
-  originalUrl: { type: String, required: true },
-  longUrl: { type: String, required: true, unique: true },
-  shortUrl: { type: String, required: true, unique: true },
-});
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: { type: String, required: true },
+    longUrl: { type: String, required: true, unique: true },
+    shortUrl: { type: String, required: true, unique: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: 60 * 60 * 24 * 3,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Url = mongoose.model("Url", urlSchema);

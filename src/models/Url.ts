0ae10eee6299393e81fc6c019ab7075727781802
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-07-07 11:35:12 +0100

    fixed auto-delete bug, links delete after 3 days

--- src/models/Url.ts
+++ src/models/Url.ts
@@ -5,6 +5,7 @@ const urlSchema = new mongoose.Schema(
     originalUrl: { type: String, required: true },
     longUrl: { type: String, required: true, unique: true },
     shortUrl: { type: String, required: true, unique: true },
+    expireAt: { type: Date, default: Date.now, index: { expires: "3d" } },
   },
   { timestamps: true }
 );
